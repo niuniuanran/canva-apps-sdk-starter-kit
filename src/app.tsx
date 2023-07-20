60820981063f295f4a6bbf79a119780ab0297815
Anran Niu <anran@canva.com>
2023-07-20 21:18:50 +1200

    Create step 1

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,21 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
-import { addNativeElement } from "@canva/design";
-import * as React from "react";
-import styles from "styles/components.css";
+import React, { useState } from 'react'
+import { ImportDataStep } from './steps/ImportDataStep'
+enum CVStep {
+  ImportData,
+  SelectTemplate,
+  OptimizeCV,
+}
 
 export const App = () => {
-  const onClick = () => {
-    addNativeElement({
-      type: "TEXT",
-      children: ["Hello world!"],
-    });
-  };
-
-  return (
-    <div className={styles.scrollContainer}>
-      <Rows spacing="2u">
-        <Text>
-          To make changes to this app, edit the{" "}
-          <code className={styles.code}>src/app.tsx</code> file, then close and
-          reopen the app in the editor to preview the changes.
-        </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
-        </Button>
-      </Rows>
-    </div>
-  );
-};
+  const [currentStep, setCurrentStep] = useState<CVStep>(CVStep.ImportData)
+  switch (currentStep) {
+    case CVStep.ImportData:
+      return (
+        <ImportDataStep
+          onDataImported={() => setCurrentStep(CVStep.SelectTemplate)}
+        />
+      )
+    case CVStep.SelectTemplate:
+      return <div>Select template</div>
+  }
+}
---
+++ src/steps/ImportDataStep.tsx
@@ -0,0 +1,58 @@
+import { Rows, Button, Text } from '@canva/app-ui-kit'
+import React, { useState } from 'react'
+import { FC } from 'react'
+import linkedin from '../assets/images/linkedin300.png'
+import cvUpload from '../assets/images/cv-upload300.png'
+
+interface Step1Props {
+  onDataImported: () => void
+}
+
+export const ImportDataStep: FC<Step1Props> = ({ onDataImported }) => {
+  const [linkedInLoading, setLinkedInLoading] = useState<boolean>(false)
+  const importFromPDF = () => {
+    let input = document.createElement('input')
+    input.type = 'file'
+    input.onchange = (_) => {
+      // i'm not really handling the file 😂
+      onDataImported()
+    }
+    input.click()
+  }
+  const importFromLinkedIn = () => {
+    setLinkedInLoading(true)
+    // i'm not really importing Linkedin 😂
+    setTimeout(() => {
+      setLinkedInLoading(false)
+      onDataImported()
+    }, 700)
+  }
+
+  return (
+    <Rows spacing="4u">
+      <Text>1. Import your CV data</Text>
+      <Rows spacing="1u" align="center">
+        <img src={cvUpload} width={100} />
+        <Button
+          variant="primary"
+          stretch
+          onClick={importFromPDF}
+          disabled={linkedInLoading}
+        >
+          Import from PDF
+        </Button>
+      </Rows>
+      <Rows spacing="1u" align="center">
+        <img src={linkedin} width={100} />
+        <Button
+          variant="primary"
+          stretch
+          onClick={importFromLinkedIn}
+          loading={linkedInLoading}
+        >
+          Import from LinkedIn
+        </Button>
+      </Rows>
+    </Rows>
+  )
+}
